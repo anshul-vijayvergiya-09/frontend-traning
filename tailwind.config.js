645b7472d19b3289e64eb61ce39a4c9ac93f7963
Anshul <anshulvijayvergiya09@gmail.com>
2026-01-29 14:21:46 +0530

    Added Progress

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./ui-foundation/**/*.{html,js}",
+    "./react-lab/**/*.{html,js}",
+    "./motion-lab/**/*.{html,js}",
+    "./playground/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
